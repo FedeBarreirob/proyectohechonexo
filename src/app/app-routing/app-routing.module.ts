443fd19fae1e7fc609241e13de53ebddbae5f730
Alberto Andrés Cattelán <albertocattelan@gmail.com>
2019-01-15 22:03:08 -0300

    Cambio Perfiles por Administrador + fixes

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -87,7 +87,7 @@ const routes: Routes = [
     }
   },
   {
-    path: 'perfiles',
+    path: 'administrador-de-cuentas',
     component: PerfilesListadoComponent,
     canActivate: [AuthGuardGuard],
     data: {
