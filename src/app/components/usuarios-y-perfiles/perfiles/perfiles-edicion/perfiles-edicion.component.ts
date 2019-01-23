ca41473de2067b35ed0589af6eacf5754c08828c
Alberto Andrés Cattelán <albertocattelan@gmail.com>
2019-01-23 01:07:27 -0300

    UI vinculación de comerciales

--- src/app/components/usuarios-y-perfiles/perfiles/perfiles-edicion/perfiles-edicion.component.ts
+++ src/app/components/usuarios-y-perfiles/perfiles/perfiles-edicion/perfiles-edicion.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
-import { MatSnackBar, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Component, OnInit, Inject, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { MatSnackBar, MatDialogRef, MAT_DIALOG_DATA, MatAutocompleteTrigger } from '@angular/material';
 import { PerfilesService } from '../../../../services/perfiles/perfiles.service';
 import { PerfilBasico } from '../../../../interfaces/perfiles/perfil-basico';
 import { PerfilBasicoCredencial } from '../../../../interfaces/perfiles/perfil-basico-credencial';
@@ -8,212 +8,280 @@ import { PerfilBasicoInfoPersonal } from '../../../../interfaces/perfiles/perfil
 import { Rol } from '../../../../interfaces/security/rol';
 import { AuthenticationService } from '../../../../services/security/authentication.service';
 import { UserAuth } from '../../../../models/security/user';
+import { RoleEnum } from '../../../../enums/role-enum.enum';
 
 @Component({
-  selector: 'app-perfiles-edicion',
-  templateUrl: './perfiles-edicion.component.html',
-  styleUrls: ['./perfiles-edicion.component.css']
+	selector: 'app-perfiles-edicion',
+	templateUrl: './perfiles-edicion.component.html',
+	styleUrls: ['./perfiles-edicion.component.css']
 })
 export class PerfilesEdicionComponent implements OnInit {
 
-  private formSubmitAttempt: boolean;
-  public guardando: boolean = false;
-  private usuarioLogueado: UserAuth;
+	private formSubmitAttempt: boolean;
+	public guardando: boolean = false;
+	private usuarioLogueado: UserAuth;
 
-  public formDatosAccesoGroup: FormGroup;
-  public formDatosPersonalesGroup: FormGroup;
-  public formCuentasVinculadasGroup: FormGroup;
+	public formDatosAccesoGroup: FormGroup;
+	public formDatosPersonalesGroup: FormGroup;
+	public formCuentasVinculadasGroup: FormGroup;
+	public formComercialesVinculadosGroup: FormGroup;
 
-  public esRegistroNuevo: boolean;
-  public titulo: string;
+	// busqueda de comerciales
+	public buscadorComercial: FormControl = new FormControl();
+	public listadoComerciales = <any>[];
+	public comercialSeleccionado: PerfilBasico = null;
+	public listadoComercialesSeleccionados = <any>[];
 
-  public listadoCodigos: string[] = [];
-  // TODO: Ver de traer los roles por servicio
-  public roles: Array<Rol> = [
-    {
-      id: 1,
-      denominacion: "Administrador",
-      admin: true
-    },
-    {
-      id: 7,
-      denominacion: "SubAdministrador",
-      admin: false
-    },
-    {
-      id: 2,
-      denominacion: "Productor",
-      admin: false
-    },
-    {
-      id: 9,
-      denominacion: "Comercial",
-      admin: false
-    }];
+	public esRegistroNuevo: boolean;
+	public titulo: string;
 
-  public perfilBasico: PerfilBasico;
+	public listadoCodigos: string[] = [];
+	// TODO: Ver de traer los roles por servicio
+	public roles: Array<Rol> = [
+		{
+			id: 1,
+			denominacion: "Administrador",
+			admin: true
+		},
+		{
+			id: 7,
+			denominacion: "SubAdministrador",
+			admin: false
+		},
+		{
+			id: 2,
+			denominacion: "Productor",
+			admin: false
+		},
+		{
+			id: 9,
+			denominacion: "Comercial",
+			admin: false
+		}];
 
-  constructor(
-    private authenticationService: AuthenticationService,
-    private formBuilder: FormBuilder,
-    private snackBar: MatSnackBar,
-    private perfilService: PerfilesService,
-    private dialogRef: MatDialogRef<PerfilesEdicionComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: PerfilBasico
-  ) {
-    this.usuarioLogueado = <UserAuth>this.authenticationService.usuarioLogueado();
-  }
+	public perfilBasico: PerfilBasico;
+	selection: any;
+	searchControl: any;
 
-  ngOnInit() {
-    // determinar si es un registro nuevo o una actualizacion de perfil
-    this.esRegistroNuevo = this.data == null ? true : false;
-    this.titulo = this.esRegistroNuevo ? "Nuevo perfil" : "Ver/Editar perfil";
+	constructor(
+		private authenticationService: AuthenticationService,
+		private formBuilder: FormBuilder,
+		private snackBar: MatSnackBar,
+		private perfilService: PerfilesService,
+		private dialogRef: MatDialogRef<PerfilesEdicionComponent>,
+		@Inject(MAT_DIALOG_DATA) public data: PerfilBasico
+	) {
+		this.usuarioLogueado = <UserAuth>this.authenticationService.usuarioLogueado();
+	}
 
-    // credenciales
-    if (this.data != null) {
-      this.formDatosAccesoGroup = this.formBuilder.group(this.data.credencial);
-      this.formDatosAccesoGroup.addControl('rol', new FormControl(this.roles.filter(x => x.id == (this.data.rol != null ? this.data.rol.id : null))[0]));
-    } else {
-      this.formDatosAccesoGroup = this.formBuilder.group({
-        username: [''],
-        password: [''],
-        passwordConfirmacion: [''],
-        rol: [null]
-      });
-    }
+	ngOnInit() {
+		// determinar si es un registro nuevo o una actualizacion de perfil
+		this.esRegistroNuevo = this.data == null ? true : false;
+		this.titulo = this.esRegistroNuevo ? "Nuevo perfil" : "Ver/Editar perfil";
 
-    // informacion personal
-    if (this.data != null) {
-      this.formDatosPersonalesGroup = this.formBuilder.group(this.data.informacionPersonal);
-    } else {
-      this.formDatosPersonalesGroup = this.formBuilder.group({
-        nombre: [''],
-        domicilio: [''],
-        telefonos: [''],
-        email: [''],
-        cuit: [''],
-        cbu: [''],
-      });
-    }
+		// credenciales
+		if (this.data != null) {
+			this.formDatosAccesoGroup = this.formBuilder.group(this.data.credencial);
+			this.formDatosAccesoGroup.addControl('rol', new FormControl(this.roles.filter(x => x.id == (this.data.rol != null ? this.data.rol.id : null))[0]));
+		} else {
+			this.formDatosAccesoGroup = this.formBuilder.group({
+				username: [''],
+				password: [''],
+				passwordConfirmacion: [''],
+				rol: [null]
+			});
+		}
 
-    // cuentas vinculadas
-    this.formCuentasVinculadasGroup = this.formBuilder.group({
-      entidadCodigo: [''],
-    });
-    if (this.data != null && this.data.entidadCodigos != null) {
-      this.listadoCodigos = Object.assign([], this.data.entidadCodigos);
-    }
-  }
+		// informacion personal
+		if (this.data != null) {
+			this.formDatosPersonalesGroup = this.formBuilder.group(this.data.informacionPersonal);
+		} else {
+			this.formDatosPersonalesGroup = this.formBuilder.group({
+				nombre: [''],
+				domicilio: [''],
+				telefonos: [''],
+				email: [''],
+				cuit: [''],
+				cbu: [''],
+			});
+		}
 
-  // funcion encargada de enviar los datos para su persistencia
-  guardar() {
-    if (!this.guardando) {
-      this.guardando = true;
+		// cuentas vinculadas
+		this.formCuentasVinculadasGroup = this.formBuilder.group({
+			entidadCodigo: [''],
+		});
+		if (this.data != null && this.data.entidadCodigos != null) {
+			this.listadoCodigos = Object.assign([], this.data.entidadCodigos);
+		}
 
-      let datosAcceso: PerfilBasicoCredencial = this.formDatosAccesoGroup.getRawValue();
-      let informacionPersonal: PerfilBasicoInfoPersonal = this.formDatosPersonalesGroup.getRawValue();
-      let rol: Rol = this.formDatosAccesoGroup.value.rol;
+		// comerciales vinculados
+		this.formComercialesVinculadosGroup = this.formBuilder.group({
+			perfilComercial: [''],
+		});
+		/*if (this.data != null && this.data.entidadCodigos != null) {
+			this.listadoCodigos = Object.assign([], this.data.entidadCodigos);
+		}*/
 
-      this.perfilBasico = {
-        credencial: datosAcceso,
-        informacionPersonal: informacionPersonal,
-        entidadCodigos: this.listadoCodigos,
-        rol: rol
-      };
+		this.buscadorComercial.valueChanges.subscribe(
+			termino => {
+				if (termino != '') {
+					this.perfilService.perfilesDeUnRolDado(RoleEnum['COMERCIAL'], termino, this.usuarioLogueado.token).subscribe(
+						respuesta => {
+							this.listadoComerciales = respuesta;
+						}
+					)
+				}
+			}
+		);
+	}
 
-      if (this.esRegistroNuevo) {
-        this.guardarNuevo();
-      } else {
-        this.guardarModificar();
-      }
-    } else {
-      this.openSnackBar("Existe un proceso de registro ejecutándose.", "Registro/Actualización del perfil");
-    }
-  }
+	// funcion encargada de enviar los datos para su persistencia
+	guardar() {
+		if (!this.guardando) {
+			this.guardando = true;
 
-  // funcion encargada de guardar un nuevo perfil
-  guardarNuevo() {
-    this.perfilService.registrarNuevo(this.perfilBasico, this.usuarioLogueado.token)
-      .subscribe(respuesta => {
+			let datosAcceso: PerfilBasicoCredencial = this.formDatosAccesoGroup.getRawValue();
+			let informacionPersonal: PerfilBasicoInfoPersonal = this.formDatosPersonalesGroup.getRawValue();
+			let rol: Rol = this.formDatosAccesoGroup.value.rol;
 
-        this.guardando = false;
+			this.perfilBasico = {
+				credencial: datosAcceso,
+				informacionPersonal: informacionPersonal,
+				entidadCodigos: this.listadoCodigos,
+				rol: rol
+			};
 
-        if (respuesta && respuesta.exito == false) {
-          this.openSnackBar(respuesta.mensaje, "Registro del perfil");
-        } else {
-          this.openSnackBar(respuesta.mensaje, "Registro del perfil");
-          this.dialogRef.close();
-        }
+			if (this.esRegistroNuevo) {
+				this.guardarNuevo();
+			} else {
+				this.guardarModificar();
+			}
+		} else {
+			this.openSnackBar("Existe un proceso de registro ejecutándose.", "Registro/Actualización del perfil");
+		}
+	}
 
-      }, error => {
-        this.guardando = false;
+	// funcion encargada de guardar un nuevo perfil
+	guardarNuevo() {
+		this.perfilService.registrarNuevo(this.perfilBasico, this.usuarioLogueado.token)
+			.subscribe(respuesta => {
 
-        this.openSnackBar("Error al intentar registrar el perfil", "Registro del perfil");
-        console.log(error);
-      });
-  }
+				this.guardando = false;
 
-  // funcion encargada de actualizar un perfil
-  guardarModificar() {
-    this.perfilService.actualizar(this.perfilBasico, this.usuarioLogueado.token)
-      .subscribe(respuesta => {
+				if (respuesta && respuesta.exito == false) {
+					this.openSnackBar(respuesta.mensaje, "Registro del perfil");
+				} else {
+					this.openSnackBar(respuesta.mensaje, "Registro del perfil");
+					this.dialogRef.close();
+				}
 
-        this.guardando = false;
+			}, error => {
+				this.guardando = false;
 
-        if (respuesta && respuesta.exito == false) {
-          this.openSnackBar(respuesta.mensaje, "Actualización del perfil");
-        } else {
-          this.openSnackBar(respuesta.mensaje, "Actualización del perfil");
-          this.dialogRef.close();
-        }
+				this.openSnackBar("Error al intentar registrar el perfil", "Registro del perfil");
+				console.log(error);
+			});
+	}
 
-      }, error => {
-        this.guardando = false;
+	// funcion encargada de actualizar un perfil
+	guardarModificar() {
+		this.perfilService.actualizar(this.perfilBasico, this.usuarioLogueado.token)
+			.subscribe(respuesta => {
 
-        this.openSnackBar("Error al intentar actualizar el perfil", "Actualización del perfil");
-        console.log(error);
-      });
-  }
+				this.guardando = false;
 
-  isCuentasVinculadasFieldInvalid(field: string) {
-    return (
-      (!this.formCuentasVinculadasGroup.get(field).valid && this.formCuentasVinculadasGroup.get(field).touched) ||
-      (this.formCuentasVinculadasGroup.get(field).untouched && this.formSubmitAttempt)
-    );
-  }
+				if (respuesta && respuesta.exito == false) {
+					this.openSnackBar(respuesta.mensaje, "Actualización del perfil");
+				} else {
+					this.openSnackBar(respuesta.mensaje, "Actualización del perfil");
+					this.dialogRef.close();
+				}
 
-  // funcion que agrega una cuenta al listado de cuentas a vincular
-  agregarCuenta() {
-    if (this.formCuentasVinculadasGroup.valid) {
-      const index: number = this.listadoCodigos.indexOf(this.formCuentasVinculadasGroup.value.entidadCodigo);
-      if (index == -1) {
-        this.listadoCodigos.unshift(this.formCuentasVinculadasGroup.value.entidadCodigo);
-        this.formCuentasVinculadasGroup.reset();
-      } else {
-        this.openSnackBar("La cuenta se encuentra en el listado", "Vinculación de cuentas");
-      }
-    } else {
-      this.openSnackBar("Indique la cuenta", "Vinculación de cuentas");
-    }
-  }
+			}, error => {
+				this.guardando = false;
 
-  // funcion encargada de quitar una cuenta del listado
-  quitarCuenta(codigo: string) {
-    try {
-      const index: number = this.listadoCodigos.indexOf(codigo);
-      if (index !== -1) {
-        this.listadoCodigos.splice(index, 1);
-      }
-    } catch (e) {
-      console.log(e);
-      this.openSnackBar("Error al desvincular la cuenta", "Vinculación de cuentas");
-    }
-  }
+				this.openSnackBar("Error al intentar actualizar el perfil", "Actualización del perfil");
+				console.log(error);
+			});
+	}
 
-  // abre una notificacion
-  openSnackBar(message: string, action: string) {
-    this.snackBar.open(message, action, {
-      duration: 2000,
-    });
-  }
+	isCuentasVinculadasFieldInvalid(field: string) {
+		return (
+			(!this.formCuentasVinculadasGroup.get(field).valid && this.formCuentasVinculadasGroup.get(field).touched) ||
+			(this.formCuentasVinculadasGroup.get(field).untouched && this.formSubmitAttempt)
+		);
+	}
+
+	// funcion que agrega una cuenta al listado de cuentas a vincular
+	agregarCuenta() {
+		if (this.formCuentasVinculadasGroup.valid) {
+			const index: number = this.listadoCodigos.indexOf(this.formCuentasVinculadasGroup.value.entidadCodigo);
+			if (index == -1) {
+				this.listadoCodigos.unshift(this.formCuentasVinculadasGroup.value.entidadCodigo);
+				this.formCuentasVinculadasGroup.reset();
+			} else {
+				this.openSnackBar("La cuenta se encuentra en el listado", "Vinculación de cuentas");
+			}
+		} else {
+			this.openSnackBar("Indique la cuenta", "Vinculación de cuentas");
+		}
+	}
+
+	// funcion encargada de quitar una cuenta del listado
+	quitarCuenta(codigo: string) {
+		try {
+			const index: number = this.listadoCodigos.indexOf(codigo);
+			if (index !== -1) {
+				this.listadoCodigos.splice(index, 1);
+			}
+		} catch (e) {
+			console.log(e);
+			this.openSnackBar("Error al desvincular la cuenta", "Vinculación de cuentas");
+		}
+	}
+
+	isComercialesVinculadosFieldInvalid(field: string) {
+		return (
+			(!this.formComercialesVinculadosGroup.get(field).valid && this.formComercialesVinculadosGroup.get(field).touched) ||
+			(this.formComercialesVinculadosGroup.get(field).untouched && this.formSubmitAttempt)
+		);
+	}
+
+	// abre una notificacion
+	openSnackBar(message: string, action: string) {
+		this.snackBar.open(message, action, {
+			duration: 2000,
+		});
+	}
+
+	// funcion encargada de capturar el comercial seleccionado
+	seleccionarComercial(comercial: PerfilBasico) {
+		this.comercialSeleccionado = comercial;
+	}
+
+	// funcion encargada de obtener el texto a mostrar cuando se seleccione un comercial
+	displayComercial(comercial: PerfilBasico) {
+		if (comercial) {
+			return comercial.informacionPersonal.nombre !== null ? comercial.informacionPersonal.nombre : comercial.credencial.username;
+		} else {
+			return '';
+		}
+	}
+
+	// funcion encargada de limpiar la seleccion
+	limpiarSeleccionComercial() {
+		if (this.buscadorComercial.value == "") {
+			this.comercialSeleccionado = null;
+		}
+	}
+
+	// funcion encargada de agregar un comercial a la lista de vinculaciones
+	agregarComercial() {
+		this.listadoComercialesSeleccionados.push(this.comercialSeleccionado);
+	}
+
+	// funcion encargada de quitar un comercial del listado de comerciales
+	quitarComercial(comercial: PerfilBasico) {
+
+	}
 }
--- src/app/services/perfiles/perfiles.service.ts
+++ src/app/services/perfiles/perfiles.service.ts
@@ -1,81 +1,112 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { PerfilBasico } from '../../interfaces/perfiles/perfil-basico';
 import { environment } from '../../../environments/environment';
 import { ResponseServicio } from '../../interfaces/varios/response-servicio';
 import { FiltroGenericoLista } from '../../interfaces/varios/filtro-generico-lista';
+import { debounceTime } from 'rxjs/internal/operators/debounceTime';
+import { map } from 'rxjs/internal/operators/map';
+
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class PerfilesService {
 
-  private urlSeguridadPerfilRegistrar = `${environment.hostSeguridad}/perfiles/registrar`;
-  private urlSeguridadPerfilListar = `${environment.hostSeguridad}/perfiles/lista`;
-  private urlSeguridadPerfilModificar = `${environment.hostSeguridad}/perfiles/modificar`;
-  private urlSeguridadPerfilLogueado = `${environment.hostSeguridad}/perfiles/perfil`;
+	private urlSeguridadPerfilRegistrar = `${environment.hostSeguridad}/perfiles/registrar`;
+	private urlSeguridadPerfilListar = `${environment.hostSeguridad}/perfiles/lista`;
+	private urlSeguridadPerfilModificar = `${environment.hostSeguridad}/perfiles/modificar`;
+	private urlSeguridadPerfilLogueado = `${environment.hostSeguridad}/perfiles/perfil`;
+	private urlSeguridadPerfilListarDeUnRol = `${environment.hostSeguridad}/perfiles/listaDeUnRol`;
 
-  constructor(private http: HttpClient) { }
+	constructor(private http: HttpClient) { }
 
-  // funcion encargada de registrar un nuevo perfil
-  registrarNuevo(nuevoPerfil: PerfilBasico, token: string) {
+	// funcion encargada de registrar un nuevo perfil
+	registrarNuevo(nuevoPerfil: PerfilBasico, token: string) {
 
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
-      })
-    };
+		const httpOptions = {
+			headers: new HttpHeaders({
+				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${token}`
+			})
+		};
 
-    let nuevoPerfilJson = JSON.stringify(nuevoPerfil);
+		let nuevoPerfilJson = JSON.stringify(nuevoPerfil);
 
-    return this.http.post<ResponseServicio>(
-      this.urlSeguridadPerfilRegistrar, nuevoPerfilJson, httpOptions);
-  }
+		return this.http.post<ResponseServicio>(
+			this.urlSeguridadPerfilRegistrar, nuevoPerfilJson, httpOptions);
+	}
 
-  // funcion que devuelve un listado paginado
-  listadoPaginado(filtro: FiltroGenericoLista, token: string) {
+	// funcion que devuelve un listado paginado
+	listadoPaginado(filtro: FiltroGenericoLista, token: string) {
 
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
-      })
-    };
+		const httpOptions = {
+			headers: new HttpHeaders({
+				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${token}`
+			})
+		};
 
-    let filtroJson = JSON.stringify(filtro);
+		let filtroJson = JSON.stringify(filtro);
 
-    return this.http.post<ResponseServicio>(
-      this.urlSeguridadPerfilListar, filtroJson, httpOptions);
-  }
+		return this.http.post<ResponseServicio>(
+			this.urlSeguridadPerfilListar, filtroJson, httpOptions);
+	}
 
-  // funcion encargada de actualizar la informacion personal y cuentas de un perfil existente
-  actualizar(perfil: PerfilBasico, token: string) {
+	// funcion encargada de actualizar la informacion personal y cuentas de un perfil existente
+	actualizar(perfil: PerfilBasico, token: string) {
 
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
-      })
-    };
+		const httpOptions = {
+			headers: new HttpHeaders({
+				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${token}`
+			})
+		};
 
-    let perfilJson = JSON.stringify(perfil);
+		let perfilJson = JSON.stringify(perfil);
 
-    return this.http.post<ResponseServicio>(
-      this.urlSeguridadPerfilModificar, perfilJson, httpOptions);
-  }
+		return this.http.post<ResponseServicio>(
+			this.urlSeguridadPerfilModificar, perfilJson, httpOptions);
+	}
 
-  // funcion que devuelve los datos del perfil que se encuentra vinculado al token actual
-  perfilLogueado(token: string) {
+	// funcion que devuelve los datos del perfil que se encuentra vinculado al token actual
+	perfilLogueado(token: string) {
 
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
-      })
-    };
+		const httpOptions = {
+			headers: new HttpHeaders({
+				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${token}`
+			})
+		};
 
-    return this.http.post<ResponseServicio>(
-      this.urlSeguridadPerfilLogueado, null, httpOptions);
-  }
+		return this.http.post<ResponseServicio>(
+			this.urlSeguridadPerfilLogueado, null, httpOptions);
+	}
+
+	// funcion que devuelve un listado de perfiles correspondiente a un rol dado
+	perfilesDeUnRolDado(rolId: any, filtro: string, token: string) {
+
+		let httpHeaders = new HttpHeaders()
+			.set('Content-Type', 'application/json')
+			.set('Authorization', `Bearer ${token}`);
+
+		let httpParams = new HttpParams()
+			.set('rolId', rolId)
+			.set('filtro', filtro);
+
+		let options = {
+			headers: httpHeaders,
+			params: httpParams
+		}
+
+		return this.http
+			.get<ResponseServicio>(this.urlSeguridadPerfilListarDeUnRol, options)
+			.pipe(debounceTime(300), map(respuesta => {
+				if (respuesta && respuesta.exito == true) {
+					return respuesta.datos;
+				} else {
+					return [];
+				}
+			}));
+	}
 }
