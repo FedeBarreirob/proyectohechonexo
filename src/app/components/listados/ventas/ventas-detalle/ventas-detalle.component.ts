ac43eb117be817694c076b905a9b66e5682437a3
Alberto Andrés Cattelán <albertocattelan@gmail.com>
2019-04-27 20:04:34 -0300

    fix build

--- src/app/components/listados/ventas/ventas-detalle/ventas-detalle.component.ts
+++ src/app/components/listados/ventas/ventas-detalle/ventas-detalle.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material';
 import { MovimientoVenta } from '../../../../interfaces/ventas/listado-ventas';
-import { VentasDetalleMasOperacionesComponent } from '../ventas-detalle-mas-operaciones/ventas-detalle-mas-operaciones.component';
 import { AuthenticationService } from '../../../../services/security/authentication.service';
 import { PerfilBasico } from '../../../../interfaces/perfiles/perfil-basico';
 import { VentasExportacionesService } from '../../../../services/ventas/ventas-exportaciones.service';
@@ -33,13 +32,6 @@ export class VentasDetalleComponent implements OnInit {
 		if (perfilBasico) {
 			this.unidadMedida = perfilBasico.informacionPersonal.unidadMedidaPeso;
 		}
-	}
-
-	// funcion que muestra las operaciones extras
-	verOpcionesExtras() {
-		this.dialog.open(VentasDetalleMasOperacionesComponent, {
-			data: this.data
-		});
 	}
 
 	// funcion encargada de cerrar el modal
