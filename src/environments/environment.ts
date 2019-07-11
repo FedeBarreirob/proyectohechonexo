76179f8bcdac074c12c33f814a20e9a8cc1fc9e1
Alberto Andrés Cattelán <albertocattelan@gmail.com>
2019-07-11 13:52:46 -0300

    fix background login + fix posición botón comprobantes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,8 +9,7 @@ export const environment = {
   hostEntregasYVentas: 'http://localhost:8080/DigitalEntregasYVentas-1.0-SNAPSHOT/api',
   hostComunicaciones: 'http://localhost:8080/Comunicaciones-1.0-SNAPSHOT/api',
   hostGeneradorComprobantes: 'http://localhost:8080/GeneradorComprobantes-1.0-SNAPSHOT/api',
-  baseUrl: 'http://localhost:4200/#',
-  urlClientesDesktopActual: 'http://clientes.gaviglio.com/'*/
+  baseUrl: 'http://localhost:4200/#'*/
   
 
   hostSeguridad: 'http://192.168.20.63:8080/DigitalSeguridad-1.0-SNAPSHOT/api',
@@ -18,8 +17,7 @@ export const environment = {
   hostEntregasYVentas: 'http://192.168.20.63:8080/DigitalEntregasYVentas-1.0-SNAPSHOT/api',
   hostComunicaciones: 'http://192.168.20.63:8080/Comunicaciones-1.0-SNAPSHOT/api',
   hostGeneradorComprobantes: 'http://192.168.20.63:8080/GeneradorComprobantes-1.0-SNAPSHOT/api',
-  baseUrl: 'http://192.168.20.63/DigitalUIClientes/#',
-  urlClientesDesktopActual: 'http://clientes.gaviglio.com/'
+  baseUrl: 'http://192.168.20.63/DigitalUIClientes/#'
 };
 
 /*
