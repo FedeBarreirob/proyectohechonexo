eac86d0acdfcdf0d07ab0378ca7cbb057bbce457
Alberto Andrés Cattelán <albertocattelan@gmail.com>
2019-07-17 19:04:10 -0300

    se agrega analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { MatIconRegistry } from '@angular/material';
 import { DomSanitizer } from '@angular/platform-browser';
+import { Router, NavigationEnd } from '@angular/router';
+import { AuthenticationService } from './services/security/authentication.service';
+
+// declare ga as a function to set and sent the events
+declare let ga: Function;
 
 @Component({
 	selector: 'app-root',
@@ -13,10 +18,51 @@ export class AppComponent {
 
 	constructor(
 		private matIconRegistry: MatIconRegistry,
-		private domSanitizer: DomSanitizer) {
+		private domSanitizer: DomSanitizer,
+		private router: Router,
+		private authService: AuthenticationService
+	) {
+		this.cargarIconos();
+		this.trackUrl();
+		this.trackUser();
+	}
+
+	/**
+	 * Carga los iconos
+	 */
+	private cargarIconos() {
 		this.matIconRegistry.addSvgIcon(
 			"pdf",
 			this.domSanitizer.bypassSecurityTrustResourceUrl("assets/pdf.svg")
+		);
+	}
+
+	/**
+	 * Trackea las url para google analytics
+	 */
+	private trackUrl() {
+		// subscribe to router events and send page views to Google Analytics
+		this.router.events.subscribe(event => {
+
+			if (event instanceof NavigationEnd) {
+				ga('set', 'page', event.urlAfterRedirects);
+				ga('send', 'pageview');
+			}
+
+		});
+	}
+
+	/**
+	 * Trackea el usuario logueado para google analytics
+	 */
+	private trackUser() {
+		this.authService.huboLoginCompleto$.subscribe(
+			respuesta => {
+				if (respuesta == true) {
+					let usuario = this.authService.perfilUsuarioLogueado().credencial.username;
+					ga('set', 'userId', usuario);
+				}
+			}
 		);
 	}
 }
