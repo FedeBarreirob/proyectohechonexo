7ecd8a3da40452cbd14eea61a3cd3d8feb2ad6b4
Alberto Andrés Cattelán <albertocattelan@gmail.com>
2019-07-25 11:18:32 -0300

    se apunta a api bajo ssl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,10 +7,10 @@ export const environment = {
   hostGeneradorComprobantes: 'http://192.168.20.63:8080/GeneradorComprobantes-1.0-SNAPSHOT/api',
   baseUrl: 'http://192.168.20.63/DigitalUIClientes/#'*/
 
-  hostSeguridad: 'http://digital.gaviglio.com:8080/DigitalSeguridad-1.0-SNAPSHOT/api',
-  hostCtaCte: 'http://digital.gaviglio.com:8080/DigitalCuentaCorriente-1.0-SNAPSHOT/api',
-  hostEntregasYVentas: 'http://digital.gaviglio.com:8080/DigitalEntregasYVentas-1.0-SNAPSHOT/api',
-  hostComunicaciones: 'http://digital.gaviglio.com:8080/Comunicaciones-1.0-SNAPSHOT/api',
-  hostGeneradorComprobantes: 'http://digital.gaviglio.com:8080/GeneradorComprobantes-1.0-SNAPSHOT/api',
-  baseUrl: 'http://digital.gaviglio.com/DigitalUIClientes/#'
+  hostSeguridad: 'https://digital.gaviglio.com:8443/DigitalSeguridad-1.0-SNAPSHOT/api',
+  hostCtaCte: 'https://digital.gaviglio.com:8443/DigitalCuentaCorriente-1.0-SNAPSHOT/api',
+  hostEntregasYVentas: 'https://digital.gaviglio.com:8443/DigitalEntregasYVentas-1.0-SNAPSHOT/api',
+  hostComunicaciones: 'https://digital.gaviglio.com:8443/Comunicaciones-1.0-SNAPSHOT/api',
+  hostGeneradorComprobantes: 'https://digital.gaviglio.com:8443/GeneradorComprobantes-1.0-SNAPSHOT/api',
+  baseUrl: 'https://digital.gaviglio.com/DigitalUIClientes/#'
 };
