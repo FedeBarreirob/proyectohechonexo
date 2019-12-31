6af3b6efd1e4e99861c75a6d7d121cb34c1cf856
Alberto Andrés Cattelán <albertocattelan@gmail.com>
2019-12-31 12:10:42 -0300

    ajustes en ui de edición de perfiles administrativa

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,13 +8,13 @@ export const environment = {
   oneSignalApiKey: '39901250-6212-4942-9d7a-413712e251fb',
   googleAnalyticsID: 'UA-144101541-1',
   trackear: false,
-  /*hostSeguridad: 'http://localhost:8080/DigitalSeguridad-1.0-SNAPSHOT/api',
+  hostSeguridad: 'http://localhost:8080/DigitalSeguridad-1.0-SNAPSHOT/api',
   hostCtaCte: 'http://localhost:8080/DigitalCuentaCorriente-1.0-SNAPSHOT/api',
   hostEntregasYVentas: 'http://localhost:8080/DigitalEntregasYVentas-1.0-SNAPSHOT/api',
   hostComunicaciones: 'http://localhost:8080/Comunicaciones-1.0-SNAPSHOT/api',
   hostGeneradorComprobantes: 'http://localhost:8080/GeneradorComprobantes-1.0-SNAPSHOT/api',
   baseUrl: 'http://localhost:4200/#'
-  */
+
 
   /*
   hostSeguridad: 'http://192.168.20.63:8080/DigitalSeguridad-1.0-SNAPSHOT/api',
@@ -24,12 +24,12 @@ export const environment = {
   hostGeneradorComprobantes: 'http://192.168.20.63:8080/GeneradorComprobantes-1.0-SNAPSHOT/api',
   baseUrl: 'http://192.168.20.63/DigitalUIClientes/#'*/
 
-  hostSeguridad: 'https://digital.gaviglio.com:8443/DigitalSeguridad-1.0-SNAPSHOT/api',
+  /*hostSeguridad: 'https://digital.gaviglio.com:8443/DigitalSeguridad-1.0-SNAPSHOT/api',
   hostCtaCte: 'https://digital.gaviglio.com:8443/DigitalCuentaCorriente-1.0-SNAPSHOT/api',
   hostEntregasYVentas: 'https://digital.gaviglio.com:8443/DigitalEntregasYVentas-1.0-SNAPSHOT/api',
   hostComunicaciones: 'https://digital.gaviglio.com:8443/Comunicaciones-1.0-SNAPSHOT/api',
   hostGeneradorComprobantes: 'https://digital.gaviglio.com:8443/GeneradorComprobantes-1.0-SNAPSHOT/api',
-  baseUrl: 'https://digital.gaviglio.com/DigitalUIClientes/#'
+  baseUrl: 'https://digital.gaviglio.com/DigitalUIClientes/#'*/
 };
 
 /*
