552f917868e9a343bdd5d9fc5d1270016dc994cf
Alberto Andrés Cattelán <albertocattelan@gmail.com>
2020-01-15 19:38:19 -0300

    apk 7

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,21 +8,21 @@ export const environment = {
   oneSignalApiKey: '39901250-6212-4942-9d7a-413712e251fb',
   googleAnalyticsID: 'UA-144101541-1',
   trackear: false,
-  hostSeguridad: 'http://localhost:8080/DigitalSeguridad-1.0-SNAPSHOT/api',
+  /*hostSeguridad: 'http://localhost:8080/DigitalSeguridad-1.0-SNAPSHOT/api',
   hostCtaCte: 'http://localhost:8080/DigitalCuentaCorriente-1.0-SNAPSHOT/api',
   hostEntregasYVentas: 'http://localhost:8080/DigitalEntregasYVentas-1.0-SNAPSHOT/api',
   hostComunicaciones: 'http://localhost:8080/Comunicaciones-1.0-SNAPSHOT/api',
   hostGeneradorComprobantes: 'http://localhost:8080/GeneradorComprobantes-1.0-SNAPSHOT/api',
-  baseUrl: 'http://localhost:4200/#'
+  baseUrl: 'http://localhost:4200/#'*/
 
 
-  /*
+  
   hostSeguridad: 'http://192.168.20.63:8080/DigitalSeguridad-1.0-SNAPSHOT/api',
   hostCtaCte: 'http://192.168.20.63:8080/DigitalCuentaCorriente-1.0-SNAPSHOT/api',
   hostEntregasYVentas: 'http://192.168.20.63:8080/DigitalEntregasYVentas-1.0-SNAPSHOT/api',
   hostComunicaciones: 'http://192.168.20.63:8080/Comunicaciones-1.0-SNAPSHOT/api',
   hostGeneradorComprobantes: 'http://192.168.20.63:8080/GeneradorComprobantes-1.0-SNAPSHOT/api',
-  baseUrl: 'http://192.168.20.63/DigitalUIClientes/#'*/
+  baseUrl: 'http://192.168.20.63/DigitalUIClientes/#'
 
   /*hostSeguridad: 'https://digital.gaviglio.com:8443/DigitalSeguridad-1.0-SNAPSHOT/api',
   hostCtaCte: 'https://digital.gaviglio.com:8443/DigitalCuentaCorriente-1.0-SNAPSHOT/api',
